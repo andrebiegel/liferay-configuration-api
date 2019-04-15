774be879ecd5a46d51e4c0180c31a0c345b37395
André Biegel <andre.biegel@unymira.com>
2019-04-15 09:26:35 +0200

    localization in config seems to only work with an addtional plugin in bnd .. in liferay

--- osgi-core-annotation/src/main/java/de/abiegel/configuration/osgi/example/ConfiguredComponentConfig.java
+++ osgi-core-annotation/src/main/java/de/abiegel/configuration/osgi/example/ConfiguredComponentConfig.java
@@ -5,7 +5,7 @@ import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 
-@ObjectClassDefinition(name = "foo-default-name", description = "Example Configuration Description" , localization="content.Language")
+@ObjectClassDefinition(name = "foo-default-name", description = "Example Configuration Description" , localization="content/Language")
 public @interface ConfiguredComponentConfig {
 
 	@AttributeDefinition(name = "foo-default-name-entry", required = true, type = AttributeType.STRING, description = "The default Name.")
