8298fe35693dd3da90cdc4b9c3547c6e917d45e8
André Biegel <andre.biegel@unymira.com>
2019-04-17 09:07:02 +0200

    java doc added

--- liferay-scoped-portlet-instance/src/main/java/de/abiegel/configuration/osgi/example/OsgiConfiguredPortletInstanceConfigurationPidMapping.java
+++ liferay-scoped-portlet-instance/src/main/java/de/abiegel/configuration/osgi/example/OsgiConfiguredPortletInstanceConfigurationPidMapping.java
@@ -4,6 +4,11 @@ import org.osgi.service.component.annotations.Component;
 
 import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
 
+/**
+ * 
+ * Seems to connect the osgi configuration with the instance the ConfigurationProviderUtil provides , without that the default value is not going to be displayed
+ *
+ */
 @Component
 public class OsgiConfiguredPortletInstanceConfigurationPidMapping implements ConfigurationPidMapping {
 
