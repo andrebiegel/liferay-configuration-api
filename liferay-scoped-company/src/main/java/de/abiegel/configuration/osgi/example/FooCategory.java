bff3112005fa1227a94902146250cd76e9bb7b50
André Biegel <andre.biegel@unymira.com>
2019-04-17 11:15:56 +0200

    testing wether the method overide changes something with the language
    property resolution

--- liferay-scoped-company/src/main/java/de/abiegel/configuration/osgi/example/FooCategory.java
+++ liferay-scoped-company/src/main/java/de/abiegel/configuration/osgi/example/FooCategory.java
@@ -28,4 +28,11 @@ public class FooCategory implements ConfigurationCategory {
 		return "third-party";
 	}
 
+	/**
+	 * What is that for ?? 
+	 */
+	@Override
+	public String getBundleSymbolicName() {
+		return "de.abiegel.configuration.osgi.example";
+	}
 }
