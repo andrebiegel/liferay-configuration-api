4dfc4547345e9ef9c5dac923909a9ecb0eacbcdc
andrebiegel <andre.biegel@icloud.com>
2026-05-15 22:11:32 +0200

    every sample has its own category

---
+++ costum-config-category/src/main/java/de/abiegel/configuration/options/FooCategory.java
@@ -0,0 +1,29 @@
+package de.abiegel.configuration.options;
+
+import com.liferay.configuration.admin.category.ConfigurationCategory;
+import org.osgi.service.component.annotations.Component;
+
+//@Component
+public class FooCategory implements ConfigurationCategory {
+
+	@Override
+	public String getCategoryKey() {
+
+		return "my-foo";
+	}
+
+	@Override
+	public String getCategorySection() {
+		return "my-foo-title";
+	}
+
+	/**
+	 * choose one of
+	 * @see https://clayui.com/docs/components/icons.html
+	 */
+	@Override
+	public String getCategoryIcon() {
+
+		return "third-party";
+	}
+}
