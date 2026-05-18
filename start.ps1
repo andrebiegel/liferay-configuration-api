96a6aafbd5ae666342fa9b26e6f6da11c504266a
andrebiegel <andre.biegel@icloud.com>
2026-05-18 10:17:13 +0200

    start up scripts

--- start.ps1
+++ start.ps1
@@ -1,4 +1,3 @@
-Invoke-WebRequest -Uri "https://repo1.maven.org/maven2/org/apache/felix/org.apache.felix.webconsole/4.3.16/org.apache.felix.webconsole-4.3.16-all.jar" -OutFile "./deploy/org.apache.felix.webconsole-4.3.16-all.jar" `
+Invoke-WebRequest -Uri "https://repo1.maven.org/maven2/org/apache/felix/org.apache.felix.webconsole/5.0.18/org.apache.felix.webconsole-5.0.18.jar" -OutFile "./deploy/org.apache.felix.webconsole-4.3.16-all.jar" `
 && mvn clean package `
-&& Get-ChildItem -Recurse -Filter *.jar -Include */target/* | Move-Item -Destination ./deploy/ `
 && docker-compose up -d
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-curl https://repo1.maven.org/maven2/org/apache/felix/org.apache.felix.webconsole/4.3.16/org.apache.felix.webconsole-4.3.16-all.jar --output ./deploy/org.apache.felix.webconsole-4.3.16-all.jar && mvn clean package && mv ./*/target/*.jar deploy/ && docker-compose up
+curl https://repo1.maven.org/maven2/org/apache/felix/org.apache.felix.webconsole/5.0.18/org.apache.felix.webconsole-5.0.18.jar--output ./deploy/org.apache.felix.webconsole-4.3.16-all.jar && mvn clean package && mv ./*/target/*.jar deploy/ && docker-compose up
